97f2089c30f27dd4e387f284ae3f77b3a076c4b7
JoseCarlosMorales <josmorbor3@alum.us.es>
2022-10-11 16:44:16 +0200

    Añado workflow

---
+++ decide/local_settings_gactions.py
@@ -0,0 +1,9 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'decide',
+        'USER': 'decide',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
