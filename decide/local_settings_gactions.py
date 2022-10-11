060853ba1b407675c136f08b448c68bbe0c9c2b4
JoseCarlosMorales <josmorbor3@alum.us.es>
2022-10-11 16:48:27 +0200

    Configuración con la base de datos

--- decide/local_settings_gactions.py
+++ decide/local_settings_gactions.py
@@ -3,6 +3,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'decide',
         'USER': 'decide',
+        'PASSWORD': 'decide',
         'HOST': 'db',
         'PORT': '5432',
     }
